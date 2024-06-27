db81c213c1b2f50796f5cd68eb63d3142db15548
Russell Stroebel <russell@manifoldsa.onmicrosoft.com>
2024-06-27 11:29:50 +0200

    Add project files.

---
+++ ECSTASSEA/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace ECSTASSEA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTest = new System.Windows.Forms.Button();
+            this.txtDataPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSerNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnTest
+            // 
+            this.BtnTest.Location = new System.Drawing.Point(123, 166);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(450, 31);
+            this.BtnTest.TabIndex = 0;
+            this.BtnTest.Text = "Test SDK Connection (To test your SDK Authentication and Data Path)";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // txtDataPath
+            // 
+            this.txtDataPath.Location = new System.Drawing.Point(265, 76);
+            this.txtDataPath.Name = "txtDataPath";
+            this.txtDataPath.Size = new System.Drawing.Size(370, 20);
+            this.txtDataPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pastel Data Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SDK Serial No";
+            // 
+            // txtSerNum
+            // 
+            this.txtSerNum.Location = new System.Drawing.Point(265, 102);
+            this.txtSerNum.Name = "txtSerNum";
+            this.txtSerNum.Size = new System.Drawing.Size(119, 20);
+            this.txtSerNum.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SDK Auth Code";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(265, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(450, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Code behind this button will show how to import a document";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSerNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDataPath);
+            this.Controls.Add(this.BtnTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.TextBox txtDataPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSerNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ECSTASSEA/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ECSTASSEA
+{
+    public partial class Form1 : Form
+    {
+
+        private string Serno = "DK198110007";
+        private string Authcde = "5635796";
+        private PasSDK.PastelPartnerSDK SDK = new PasSDK.PastelPartnerSDK();
+        private string StrReturn;
+
+        //Sage50IntegrationMethods SG = new Sage50IntegrationMethods();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            if (txtDataPath.Text.ToString().Trim().Length < 1)
+            {
+                MessageBox.Show("No data path entered");
+            }
+            SDK.SetLicense(Serno, Authcde);
+           
+            StrReturn = SDK.SetDataPath(txtDataPath.Text);
+            if (StrReturn.StartsWith("0"))
+            {
+                MessageBox.Show(GetResultDesc(StrReturn.Split('|')[0]));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Refer to the Partner SDK ==> SDK Calls ==> ImportDocument document for additional details 
+
+            SDK.SetLicense(Serno, Authcde);
+            
+            //Set Pastel Data Path
+            StrReturn = SDK.SetDataPath(txtDataPath.Text);
+
+
+            //If you do not trap the returncode the SDK will try to use your application path, i.e. where your project files are
+            if (StrReturn == "0")
+            {
+                //Document procedures generally require you to build a header and then n number of lines depending on your line numbers
+                // 1) Open Document Files
+                if (StrReturn == "0") { SDK.OpenDocumentFiles(); };
+
+                // Refer to the Partner SDK ==> SDK Calls ==> DefineDocumentHeader document for additional details
+                // 2) Define the document Header (construct the string)
+                if (StrReturn == "0")
+                {
+                    //StrReturn = SDK.DefineDocumentHeader("||| Customer Code |Date|Order Number|Incl/Excl|Discount|Message no.1|Message no.2|Message no.3|Delivery no.1|Delivery no.2|Delivery no.3|Delivery no.4|Delivery no.5||00||Closing Date|Telephone|Contact||Ex Rate", true);
+                    StrReturn = SDK.DefineDocumentHeader("||Y|ACK001|05/03/1999||N|0|Message no.1|Message no.2|Message no.3|Delivery no.1|Delivery no.2|Delivery no.3|Delivery no.4|Delivery no.5||00||05/03/1999|011-7402156|Johnny|011-7402157|1", true);
+                }
+                // 3) Define the document Lines -  (construct strings) + You need to catch errors when defining each line
+                if (StrReturn == "0")
+                {
+                    // Refer to the Partner SDK ==> SDK Calls ==> DefineDocumentLine  document for additional details
+                    //SDK.DefineDocumentLine("Cost Price| Quantity|Unit Selling Price|Inclusive Price|Unit |Tax Type|Discount Type|Discount Percentage| Code| Description |Line Type|Multi-store");
+                    SDK.DefineDocumentLine("285|1|350.88|400.00|EACH|01|||ACC/LOC|Description|4|001");
+                    SDK.DefineDocumentLine("25.21|2|87.72|100.00|EACH|01|||COM/PCA|Description|4|001");
+                    SDK.DefineDocumentLine("0|1|87.72|100.00|EACH|01|||3150000|Description|6|001");
+                }
+
+                // 4) Import the document
+                // SDK.ImportDocument(Document Type, UserID (Optional));
+                SDK.ImportDocument(103, 0);
+                //Refer to the Partner SDK ==> Using the SDK ==> Document Types  document for additional details
+
+                //5) Close Document files
+                SDK.CloseDocumentFiles();
+            }
+        }
+
+        public string GetResultDesc(object strRCode)
+        {
+            string GetResultDescRet = default;
+            switch (strRCode)
+            {
+                case "0":
+                    {
+                        GetResultDescRet = "0 = Call successfully executed" + '\r' + '\n';
+                        break;
+                    }
+
+                case "1":
+                    {
+                        GetResultDescRet = "1 = File Not found" + '\r' + '\n';
+                        break;
+                    }
+
+                case "2":
+                    {
+                        GetResultDescRet = "2 = Invalid number of fields" + '\r' + '\n';
+                        GetResultDescRet = GetResultDescRet + "Check your SetDataPath call and make sure" + '\r' + '\n';
+                        GetResultDescRet = GetResultDescRet + "pastel files exist at that path" + '\r' + '\n';
+                        GetResultDescRet = GetResultDescRet + "Try and use directories less than 8 characters long" + '\r' + '\n';
+                        break;
+                    }
+
+                case "3":
+                    {
+                        GetResultDescRet = "3 = Record update not successful" + '\r' + '\n';
+                        break;
+                    }
+
+                case "4":
+                    {
+                        GetResultDescRet = "4 = Record insert not successful" + '\r' + '\n';
+                        break;
+                    }
+
+                case "5":
+                    {
+                        GetResultDescRet = "5 = Record does not exist in file";
+                        break;
+                    }
+
+                case "6":
+                    {
+                        GetResultDescRet = "6 = Data path does not exist";
+                        break;
+                    }
+
+                case "7":
+                    {
+                        GetResultDescRet = "7 = Access denied";
+                        break;
+                    }
+
+                case "9":
+                    {
+                        GetResultDescRet = "9 = End of file";
+                        break;
+                    }
+
+                case "10":
+                    {
+                        GetResultDescRet = "10 = Field number specified not valid";
+                        break;
+                    }
+
+                case "11":
+                    {
+                        GetResultDescRet = "11 = Invalid period number (1 to 13)";
+                        break;
+                    }
+
+                case "12":
+                    {
+                        GetResultDescRet = "12 = Invalid Date";
+                        break;
+                    }
+
+                case "13":
+                    {
+                        GetResultDescRet = "13 = Invalid account type (GDC)";
+                        break;
+                    }
+
+                case "14":
+                    {
+                        GetResultDescRet = "14 = Invalid general ledger account number";
+                        break;
+                    }
+
+                case "15":
+                    {
+                        GetResultDescRet = "15 = General ledger account contains sub accounts";
+                        break;
+                    }
+
+                case "16":
+                    {
+                        GetResultDescRet = "16 = General ledger account number must be numeric";
+                        break;
+                    }
+
+                case "17":
+                    {
+                        GetResultDescRet = "17 = Invalid customer account code";
+                        break;
+                    }
+
+                case "18":
+                    {
+                        GetResultDescRet = "18 = Invalid supplier account code";
+                        break;
+                    }
+
+                case "19":
+                    {
+                        GetResultDescRet = "19 = Invalid inventory item code";
+                        break;
+                    }
+
+                case "20":
+                    {
+                        GetResultDescRet = "20 = Invalid salesman code";
+                        break;
+                    }
+
+                case "21":
+                    {
+                        GetResultDescRet = "21 = Invalid job code";
+                        break;
+                    }
+
+                case "22":
+                    {
+                        GetResultDescRet = "22 = Invalid Tax Type (0 to 30)";
+                        break;
+                    }
+
+                case "23":
+                    {
+                        GetResultDescRet = "23 = Transaction amount cannot be less that the tax amount";
+                        break;
+                    }
+
+                case "24":
+                    {
+                        GetResultDescRet = "24 = Invalid open item transaction type - must be O (Original) or A (Allocation)";
+                        break;
+                    }
+
+                case "25":
+                    {
+                        GetResultDescRet = "25 = There cannot be more than 500 lines in a batch";
+                        break;
+                    }
+
+                case "26":
+                    {
+                        GetResultDescRet = "26 = Invalid account description";
+                        break;
+                    }
+
+                case "27":
+                    {
+                        GetResultDescRet = "27 = Default group needs to set up in Pastel";
+                        break;
+                    }
+
+                case "28":
+                    {
+                        GetResultDescRet = "28 = Invalid document line type – must be 2, 5, or 7";
+                        break;
+                    }
+
+                case "29":
+                    {
+                        GetResultDescRet = "29 = Invalid exclusive / inclusive – must be 0 or 1";
+                        break;
+                    }
+
+                case "30":
+                    {
+                        GetResultDescRet = "30 = Invalid Entry Type (1 to 90)";
+                        break;
+                    }
+
+                case "31":
+                    {
+                        GetResultDescRet = "31 = Duplicate inventory item";
+                        break;
+                    }
+
+                case "32":
+                    {
+                        GetResultDescRet = "32 = Invalid multi-store code";
+                        break;
+                    }
+
+                case "33":
+                    {
+                        GetResultDescRet = "33 = Invalid Currency Code";
+                        break;
+                    }
+
+                case "99":
+                    {
+                        GetResultDescRet = "99 = General Error (the error message is the VB 5 error code and description)";
+                        break;
+                    }
+            }
+
+            return GetResultDescRet;
+        }
+    }
+}
